c16325340d216d22431b7bb5d327e158c1fb4c46
Rafael <devrafalcoelho@gmail.com>
2023-10-11 12:22:04 -0300

    add filters clients

--- records-system/src/pages/Clients/index.tsx
+++ records-system/src/pages/Clients/index.tsx
@@ -1,6 +1,7 @@
-import { Container } from '@mui/material';
+import { Container, Button, Box } from '@mui/material';
 import Header from '../../components/Header';
 import CustomPaginationActionsTable2 from '../../components/Tables/TableListClients';
+import SearchInput from '../../components/SearchInput';
 
 export default function Clients() {
   return (
@@ -11,10 +12,18 @@ export default function Clients() {
         sx={{
           minHeight: '80vh',
           display: 'flex',
+          flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center'
         }}
       >
+        <Box
+          sx={{ marginBottom: '2vh', marginLeft: 'auto', display: 'flex', alignItems: 'center' }}
+        >
+          <Button variant="contained">Add Client</Button>
+          <SearchInput />
+        </Box>
+
         <CustomPaginationActionsTable2 />
       </Container>
     </>
